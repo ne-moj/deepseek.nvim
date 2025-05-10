18ef8e7a1f6c2ed362f99e17c72b4ad02b650258
Sergey Krasovsky <sergey.krasowski@gmail.com>
2025-05-10 13:31:20 +0200

    Add translate_code function in API

--- lua/deepseek/init.lua
+++ lua/deepseek/init.lua
@@ -5,20 +5,21 @@ local commands = require("deepseek.commands")
 local api = require("deepseek.api")
 
 function M.setup(user_opts)
-  config.setup(user_opts)
-  local cfg = config.get_config()
-  
-  -- 初始化API模块
-  api.setup(cfg)
-  
-  -- 设置命令
-  commands.setup()
+	config.setup(user_opts)
+	local cfg = config.get_config()
+
+	-- 初始化API模块
+	api.setup(cfg)
+
+	-- 设置命令
+	commands.setup()
 end
 
 -- 导出API功能
 M.generate_code = api.generate_code
 M.optimize_code = api.optimize_code
 M.analyze_code = api.analyze_code
+M.translate_code = api.translate_code
 M.chat = api.chat
 
 return M
